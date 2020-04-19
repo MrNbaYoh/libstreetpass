c4c4460a21a86f54f0a1773c254331596d2635de
MrNbaYoh <mrnbayoh@gmail.com>
2020-04-19 03:54:47 +0200

    Wrap msg functions into a class.

--- include/nl80211/commands.hpp
+++ include/nl80211/commands.hpp
@@ -2,17 +2,22 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <memory>
 #include <netlink/genl/genl.h>
 #include <linux/nl80211.h>
 
 namespace nl80211::commands {
-  void nla_put(nl_msg* msg, nl80211_attrs attr, std::uint32_t v);
-  void nla_put(nl_msg* msg, nl80211_attrs attr, std::uint16_t v);
-  void nla_put(nl_msg* msg, nl80211_attrs attr, std::uint8_t v);
-  void nla_put(nl_msg* msg, nl80211_attrs attr);
-  void nla_put(nl_msg* msg, nl80211_attrs attr, std::vector<std::uint8_t> v);
-  void nla_put(nl_msg* msg, nl80211_attrs attr, std::string s);
-
-  nl_msg* init_msg(nl80211_commands cmd, int driver_id, std::uint32_t if_idx);
-  void send_and_free_msg(nl_sock* sock, nl_msg* msg);
+  class Message {
+  private:
+    std::unique_ptr<nl_msg, decltype(&nlmsg_free)> m_nl_msg;
+  public:
+    Message(nl80211_commands cmd, int driver_id, std::uint32_t id_idx);
+    void put(nl80211_attrs attr, std::uint32_t v);
+    void put(nl80211_attrs attr, std::uint16_t v);
+    void put(nl80211_attrs attr, std::uint8_t v);
+    void put(nl80211_attrs attr);
+    void put(nl80211_attrs attr, std::vector<std::uint8_t> v);
+    void put(nl80211_attrs attr, std::string s);
+    void send(nl_sock* sock);
+  };
 }
--- src/nl80211/commands.cpp
+++ src/nl80211/commands.cpp
@@ -1,70 +1,68 @@
 #include "nl80211/commands.hpp"
 
 namespace nl80211::commands {
-  void msg_put(nl_msg* msg, nl80211_attrs attr, std::uint32_t v) {
-    int res = nla_put_u32(msg, attr, v);
+
+  void Message::put(nl80211_attrs attr, std::uint32_t v) {
+    int res = nla_put_u32(m_nl_msg.get(), attr, v);
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  void msg_put(nl_msg* msg, nl80211_attrs attr, std::uint16_t v) {
-    int res = nla_put_u16(msg, attr, v);
+  void Message::put(nl80211_attrs attr, std::uint16_t v) {
+    int res = nla_put_u16(m_nl_msg.get(), attr, v);
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  void msg_put(nl_msg* msg, nl80211_attrs attr, std::uint8_t v) {
-    int res = nla_put_u8(msg, attr, v);
+  void Message::put(nl80211_attrs attr, std::uint8_t v) {
+    int res = nla_put_u8(m_nl_msg.get(), attr, v);
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  void msg_put(nl_msg* msg, nl80211_attrs attr) {
-    int res = nla_put_flag(msg, attr);
+  void Message::put(nl80211_attrs attr) {
+    int res = nla_put_flag(m_nl_msg.get(), attr);
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  void msg_put(nl_msg* msg, nl80211_attrs attr, std::vector<std::uint8_t> v) {
-    int res = nla_put(msg, attr, v.size(), v.data());
+  void Message::put(nl80211_attrs attr, std::vector<std::uint8_t> v) {
+    int res = nla_put(m_nl_msg.get(), attr, v.size(), v.data());
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  void msg_put(nl_msg* msg, nl80211_attrs attr, std::string s) {
-    int res = nla_put(msg, attr, s.size(), s.c_str());
+  void Message::put(nl80211_attrs attr, std::string s) {
+    int res = nla_put(m_nl_msg.get(), attr, s.size(), s.c_str());
     if(res < 0)
       //TODO: better exception
       throw "nla_put";
   }
 
-  nl_msg* msg_init(nl80211_commands cmd, int driver_id, std::uint32_t if_idx) {
-    nl_msg* msg = nlmsg_alloc();
-    if(msg == nullptr)
+  Message::Message(nl80211_commands cmd, int driver_id, std::uint32_t if_idx) :
+    m_nl_msg(nlmsg_alloc(), nlmsg_free)
+  {
+    if(m_nl_msg.get() == nullptr)
       throw std::bad_alloc();
 
-    void* p_res = genlmsg_put(msg, 0, 0, driver_id, 0, 0, NL80211_CMD_JOIN_IBSS, 0);
+    void* p_res = genlmsg_put(m_nl_msg.get(), 0, 0, driver_id, 0, 0, NL80211_CMD_JOIN_IBSS, 0);
     if(p_res == nullptr)
       //TODO: better exception
       throw "genlmsg_put";
-    msg_put(msg, NL80211_ATTR_IFINDEX, if_idx);
+    put(NL80211_ATTR_IFINDEX, if_idx);
   }
 
-  void msg_send_free(nl_sock* sock, nl_msg* msg) {
-    int ret = nl_send_auto(sock, msg);
+  void Message::send(nl_sock* sock) {
+    int ret = nl_send_auto(sock, m_nl_msg.get());
   	if(ret < 0)
-  	{
-      nlmsg_free(msg);
       //TODO: better exception
       throw "nl_send_auto";
-    }
 
   	nl_recvmsgs_default(sock);
-  	nlmsg_free(msg);
   }
 }
