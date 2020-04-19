2f32648d1b0a2a9eb96cd77e7b5f2f5eea2e8a15
MrNbaYoh <mrnbayoh@gmail.com>
2020-04-19 03:57:15 +0200

    Remove all msg copy/move operators for now.

--- include/nl80211/commands.hpp
+++ include/nl80211/commands.hpp
@@ -12,6 +12,12 @@ namespace nl80211::commands {
     std::unique_ptr<nl_msg, decltype(&nlmsg_free)> m_nl_msg;
   public:
     Message(nl80211_commands cmd, int driver_id, std::uint32_t id_idx);
+
+    Message(const Message&) = delete;
+    Message& operator=(const Message&) = delete;
+    Message(Message&&) = delete;
+    Message& operator=(Message&&) = delete;
+
     void put(nl80211_attrs attr, std::uint32_t v);
     void put(nl80211_attrs attr, std::uint16_t v);
     void put(nl80211_attrs attr, std::uint8_t v);
