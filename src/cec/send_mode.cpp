5a85dcb8d71934ef1e4e57a7091afa33accb58bb
MrNbaYoh <mrnbayoh@gmail.com>
2020-09-22 05:30:57 +0200

    clang-format

--- src/cec/send_mode.cpp
+++ src/cec/send_mode.cpp
@@ -3,42 +3,40 @@
 #include <iomanip>
 
 namespace streetpass::cec {
-  bool SendMode::match(SendMode const& other) const {
-    switch (m_value) {
-      case EXCHANGE:
-        return other == EXCHANGE;
-      case RECV_ONLY:
-        return other == SEND_ONLY ||
-          other == SEND_RECV;
-      case SEND_ONLY:
-        return other == RECV_ONLY ||
-          other == SEND_RECV;
-      case SEND_RECV:
-        return other != EXCHANGE;
-      default:
-        return false;
-    }
+bool SendMode::match(SendMode const& other) const {
+  switch (m_value) {
+    case EXCHANGE:
+      return other == EXCHANGE;
+    case RECV_ONLY:
+      return other == SEND_ONLY || other == SEND_RECV;
+    case SEND_ONLY:
+      return other == RECV_ONLY || other == SEND_RECV;
+    case SEND_RECV:
+      return other != EXCHANGE;
+    default:
+      return false;
   }
+}
 
-  SendMode::operator std::string() const {
-    switch (m_value) {
-      case EXCHANGE:
-        return "EXCHANGE";
-      case RECV_ONLY:
-        return "RECV_ONLY";
-      case SEND_ONLY:
-        return "SEND_ONLY";
-      case SEND_RECV:
-        return "SEND_RECV";
-      default:
-        return "invalid";
-    }
+SendMode::operator std::string() const {
+  switch (m_value) {
+    case EXCHANGE:
+      return "EXCHANGE";
+    case RECV_ONLY:
+      return "RECV_ONLY";
+    case SEND_ONLY:
+      return "SEND_ONLY";
+    case SEND_RECV:
+      return "SEND_RECV";
+    default:
+      return "invalid";
   }
+}
 
-  std::ostream& operator<<(std::ostream& s, const SendMode& m) {
-    std::stringstream ss;
-    ss << std::setfill('0');
-    ss << std::hex << std::string(m) << "(" << std::setw(2) << m.m_value << ")";
-    return s << ss.str();
-  }
+std::ostream& operator<<(std::ostream& s, const SendMode& m) {
+  std::stringstream ss;
+  ss << std::setfill('0');
+  ss << std::hex << std::string(m) << "(" << std::setw(2) << m.m_value << ")";
+  return s << ss.str();
+}
 }  // namespace streetpass::cec
