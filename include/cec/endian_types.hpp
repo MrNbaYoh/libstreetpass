0e4e7094ad59797a0edf78a1a4b7336e7d9769ce
MrNbaYoh <mrnbayoh@gmail.com>
2020-09-25 05:19:23 +0200

    Add wrapper type definitions for little&big endian signed integers

--- include/cec/endian_types.hpp
+++ include/cec/endian_types.hpp
@@ -40,4 +40,14 @@ using u64le = EndianType<uint64_t, LITTLE>;
 using u16be = EndianType<uint16_t, BIG>;
 using u32be = EndianType<uint32_t, BIG>;
 using u64be = EndianType<uint64_t, BIG>;
+
+using i8 = int8_t;
+
+using i16le = EndianType<int16_t, LITTLE>;
+using i32le = EndianType<int32_t, LITTLE>;
+using i64le = EndianType<int64_t, LITTLE>;
+
+using i16be = EndianType<int16_t, BIG>;
+using i32be = EndianType<int32_t, BIG>;
+using i64be = EndianType<int64_t, BIG>;
 }  // namespace streetpass::cec::endian_types
